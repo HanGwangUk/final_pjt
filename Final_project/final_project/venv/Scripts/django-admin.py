a51fac6dec46cae8882e64f5312f5636c79ab89f
HanGwangUk <baramukki@naver.com>
2020-06-12 13:40:44 +0900

    DATA

--- Final_project/final_project/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\baram\desktop\공부\ssafy\final_pjt\final_project\final_project\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
